d307fcb903f3e5dc0889821b961964b61ab25403
Saket Singh <93044592+SAKETSINGH120@users.noreply.github.com>
2025-11-09 15:46:49 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url , String branch){
   echo "This is Code cloning"
-  git url : "${url}" , branch: ${"branch"}
+  git url : "${url}" , branch: "${branch}"
   echo "Code cloning successfully"
 }
